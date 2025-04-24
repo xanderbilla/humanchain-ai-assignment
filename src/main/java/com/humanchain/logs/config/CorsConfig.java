923e48a5a6dfbe4c3b80ec88b115844133b6088e
Vikas <xanderbilla@gmail.com>
2025-04-25 03:09:38 +0530

    added cors configuration

---
+++ src/main/java/com/humanchain/logs/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.humanchain.logs.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${spring.security.cors.allowed-origins}")
+    private String allowedOrigins;
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(allowedOrigins.split(","))
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Authorization", "Content-Type")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
